d3e03c3608f9bf1dd418829d35b1862d74025a60
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-17 16:43:42 -0700

    refactoed app and timer to use props system

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,36 @@ import React, { Component } from "react";
 import Timer from "./Timer";
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      timerValue: 0.5 * 60000
+    };
+    this.interval = undefined;
+  }
+
+  // Event Handlers
+  startTimer = () => {
+    this.setState({ timerValue: this.state.timerValue - 1000 });
+
+    this.interval = setInterval(() => {
+      this.setState({ timerValue: this.state.timerValue - 1000 });
+    }, 1000);
+  };
+
+  // Life Cycle Methods
+  componentDidUpdate() {
+    if (this.state.timerValue === 0) {
+      clearInterval(this.interval);
+    }
+  }
+
   render() {
     return (
       <div>
-        <Timer />
+        <Timer time={this.state.timerValue} />
+        <button onClick={this.startTimer}>Start</button>
       </div>
     );
   }
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,44 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class Timer extends Component {
-  state = {
-    interval: null,
-    timerValue: 0.5 * 60000
-  };
-
-  componentDidUpdate() {
-    if (this.state.timerValue === 0) {
-      clearInterval(this.state.interval);
-    }
-  }
-
-  millisecondsToFormatedTime(milliseconds) {
-    const minutes = Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((milliseconds % (1000 * 60)) / 1000);
-
-    const formatedMinutes = `0${minutes}`.slice(-2);
-    const formatedSeconds = `0${seconds}`.slice(-2);
-
-    return `${formatedMinutes}:${formatedSeconds}`;
-  }
-
-  startTimer = () => {
-    this.setState({ timerValue: this.state.timerValue - 1000 });
-
-    const interval = setInterval(() => {
-      this.setState({ timerValue: this.state.timerValue - 1000 });
-    }, 1000);
-    this.setState({ interval: interval });
-  };
-
-  render() {
-    return (
-      <div>
-        <p>Time: {this.millisecondsToFormatedTime(this.state.timerValue)}</p>
-        <button onClick={this.startTimer}>Start</button>
-      </div>
-    );
-  }
+export default function Timer(props) {
+  return (
+    <div>
+      <h1>{millisecondsToFormatedTime(props.time)}</h1>
+    </div>
+  );
 }
 
-export default Timer;
+const millisecondsToFormatedTime = milliseconds => {
+  const minutes = Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((milliseconds % (1000 * 60)) / 1000);
+
+  const formatedMinutes = `0${minutes}`.slice(-2);
+  const formatedSeconds = `0${seconds}`.slice(-2);
+
+  return `${formatedMinutes}:${formatedSeconds}`;
+};
