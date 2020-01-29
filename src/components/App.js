0d19bd1e168876d610945d930e13d58644adf21f
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-29 15:15:41 -0700

    start styling for app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import TimerController from "./TimerController";
+import Header from "./Header";
+import "../scss/App.scss";
 
 export default function App() {
   return (
-    <div>
+    <div className="app">
+      <Header />
       <TimerController />
     </div>
   );
