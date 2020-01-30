cce47237d294e8337d9c82d8fb38f091eb2bc216
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-30 08:22:40 -0700

    added TimerForm to toolbar and set initial values

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import HamburgerButton from "./HamburgurButton";
+import TimerForm from "./TimerForm";
 import "../scss/Toolbar.scss";
 
 class Toolbar extends Component {
   menuRef = React.createRef();
 
   toggleMenu = () => {
-    const opacity = this.menuRef.current.style.opacity;
-    this.menuRef.current.style.opacity = opacity === "0" ? "1" : "0";
+    // const opacity = this.menuRef.current.style.opacity;
+    // const display = this.menuRef.current.style.display;
+    // this.menuRef.current.style.display = display === "none" ? "block" : "none";
+    // this.menuRef.current.style.opacity = opacity === "0" ? "1" : "0";
   };
 
   render() {
@@ -22,12 +25,9 @@ class Toolbar extends Component {
         <div
           ref={this.menuRef}
           className="toolbar__menu"
-          style={{ opacity: "0" }}
+          // style={{opacity: "0", display: "none" }}
         >
-          <label>Work:</label>
-          <input type="range" min="1" max="59" step="1" />
-          <label>Break:</label>
-          <input type="range" min="1" max="59" step="1" />
+          <TimerForm initialValues={{ work: 25, break: 5, rounds: 2 }} />
         </div>
         <div className="toolbar__footer"></div>
       </div>
