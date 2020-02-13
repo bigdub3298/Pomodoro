a6a208f4fa7918df53e5687615e6aaea34e37377
bigdub3298 <wesleyaustin33@gmail.com>
2020-02-13 07:47:39 -0700

    added ability to change timer values and only reset current timer iteration if the timer amount for the current timer type changed

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,21 +1,49 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { resetTimer, stopTimer } from "../actions";
+import { setTimer, stopTimer } from "../actions";
 import HamburgerButton from "./HamburgurButton";
 import TimerForm from "./TimerForm";
 import "../scss/Toolbar.scss";
 
 class Toolbar extends Component {
   state = { menuOpen: false };
+  prevWorkTimerAmount = 0;
+  prevBreakTimerAmount = 0;
 
   toggleMenu = () => {
     if (this.state.menuOpen) {
-      this.props.resetTimer(this.props.workTime);
+      this.verifyTimerAmountChanges();
     } else {
-      this.props.stopTimer();
+      this.gatherPreviousTimerAmounts();
     }
     this.setState({ menuOpen: !this.state.menuOpen });
   };
+
+  gatherPreviousTimerAmounts() {
+    this.prevWorkTimerAmount = this.props.workTimerAmount;
+    this.prevBreakTimerAmount = this.props.breakTimerAmount;
+  }
+
+  verifyTimerAmountChanges() {
+    if (
+      this.props.type === "work" &&
+      this.prevWorkTimerAmount !== this.props.workTimerAmount
+    ) {
+      this.resetCurrentTimerIteration(this.props.workTimerAmount);
+    } else if (
+      this.props.type === "break" &&
+      this.prevBreakTimerAmount !== this.props.breakTimerAmount
+    ) {
+      this.resetCurrentTimerIteration(this.props.breakTimerAmount);
+    }
+  }
+
+  resetCurrentTimerIteration(timerAmount) {
+    if (this.props.timerIsOn) {
+      this.props.stopTimer();
+    }
+    this.props.setTimer(timerAmount);
+  }
 
   render() {
     return (
@@ -42,12 +70,21 @@ class Toolbar extends Component {
 }
 
 const mapStateToProps = state => {
-  const { work: workTime } = state.form.timerForm?.values ?? { work: 0 };
+  const { work: workTimerAmount, break: breakTimerAmount } = state.form
+    .timerForm?.values ?? {
+    work: 0,
+    break: 0
+  };
 
-  return { workTime };
+  return {
+    type: state.timer.type,
+    timerIsOn: state.timer.isTimerOn,
+    workTimerAmount,
+    breakTimerAmount
+  };
 };
 
 export default connect(mapStateToProps, {
   stopTimer,
-  resetTimer
+  setTimer
 })(Toolbar);
