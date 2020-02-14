a75a841e2cb5488d4a93197156adf4252169adac
bigdub3298 <wesleyaustin33@gmail.com>
2020-02-14 00:04:30 -0700

    Updates

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { reducer as formReducer } from "redux-form";
-import timerReducer from "./timerReducer";
-
-export default combineReducers({
-  timer: timerReducer,
-  form: formReducer
-});
